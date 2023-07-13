ebbf550af0efb68cca636d4b092b5c6439121344
Aryan Bansod <aryanem09@gmail.com>
2023-07-13 20:41:18 +0530

    done with pages now routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from "react";
 import "./app.css";
-// import Settings from "./components/Settings/Settings";
-import Login from "./components/pages/Home/Login/Login";
-// import { Settings } from "@mui/icons-material";
-// import { Settings } from "@mui/icons-material";
-// import Sidebar from "./components/Sidebar/Sidebar";
+
+import Signup from "./components/pages/Signup/Signup";
 function App() {
   return (
     <div className="App">
-      <Login />
+      <Signup />
     </div>
   );
 }
--- src/components/pages/Signup/Signup.jsx
+++ src/components/pages/Signup/Signup.jsx
@@ -1,26 +1,28 @@
 import React from "react";
-import "./login.css";
-import Navbar from "../../../topbar/Navbar";
-function Login() {
+import "./signup.css";
+import Navbar from "../../topbar/Navbar";
+function Signup() {
   return (
     <>
       <Navbar />
-      <div className="login">
-        <span className="loginTitle">Login</span>
-        <form action="" className="loginForm">
+      <div className="register">
+        <span className="registerTitle">Register</span>
+        <form action="" className="registerForm">
+          <span style={{ fontSize: "1.8rem" }}>Name</span>
+          <input type="text" placeholder="Enter your full name" />
           <span style={{ fontSize: "1.8rem" }}>Username</span>
           <input type="text" placeholder="Enter your username" />
           <span style={{ fontSize: "1.8rem" }}>Email</span>
           <input type="email" placeholder="Enter your emial" />
           <span style={{ fontSize: "1.8rem" }}>Password</span>
           <input type="password" />
-          <button className="loginBtn">Login</button>
+          <button className="registerBtn">Register</button>
         </form>
         <span>Don't Have an account ? </span>
-        <button className="registerBtn">Register</button>
+        {/* <button className="registerBtn">L</button> */}
       </div>
     </>
   );
 }
 
-export default Login;
+export default Signup;
